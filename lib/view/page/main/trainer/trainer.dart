a2a3cdc6053a1c333e270fb9aaa74d0b8eec1eca
YS-Jeong-99 <besthcy9908@gmail.com>
2022-06-30 22:37:07 +0900

    Add trainer.dart

--- lib/view/page/main/trainer/trainer.dart
+++ lib/view/page/main/trainer/trainer.dart
@@ -1,6 +1,6 @@
-import 'package:fitween1/model/user/user.dart';
-import 'package:fitween1/view/page/main/widget.dart';
+import 'package:fitween1/view/widget/text.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 // 트레이너 메인 페이지
 class TrainerMainPage extends StatelessWidget {
@@ -9,8 +9,106 @@ class TrainerMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MainAppBar(role: Role.trainer),
-      body: Container(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.chevron_left,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    iconSize: 40,
+                  ),
+                ),
+                Container(
+                  // color: Colors.green,
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width - 150,
+                  child: FWText(
+                    '삼손',
+                    size: 20,
+                  ),
+                ),
+                Container(
+                  // color: Colors.red,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.chevron_right,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    iconSize: 40,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // CircularPercentIndicator(
+          //   radius: 50.0,
+          //   lineWidth: 10.0,
+          //   percent: 0.2,
+          //   header: const Text("Icon header"),
+          //   center: Image.network(
+          //     'https://t1.daumcdn.net/cfile/tistory/240814485574155029',
+          //   ),
+          //   backgroundColor: Colors.grey,
+          //   progressColor: Colors.blue,
+          // ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Card(
+              child: SizedBox(
+                height: 120,
+                child: ListView(
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        CircularPercentIndicator(
+                          radius: 50.0,
+                          lineWidth: 10.0,
+                          percent: 0.8,
+                          center: ClipOval(
+                            child: SizedBox.fromSize(
+                              size: const Size.fromRadius(40), // Image radius
+                              child: Image.network(
+                                  'https://www.walkerhillstory.com/wp-content/uploads/2020/09/2-1.jpg',
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                          reverse: true,
+                          backgroundColor: Colors.grey,
+                          progressColor: Colors.blue,
+                        ),
+                      ],
+                    ),
+                    // Card(
+                    //   child: CircularPercentIndicator(
+                    //     radius: 30.0,
+                    //     lineWidth: 10.0,
+                    //     percent: 0.8,
+                    //     header: const Text("Icon header"),
+                    //     center: const Icon(
+                    //       Icons.person_pin,
+                    //       size: 50.0,
+                    //       color: Colors.blue,
+                    //     ),
+                    //     backgroundColor: Colors.grey,
+                    //     progressColor: Colors.blue,
+                    //   ),
+                    // ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
