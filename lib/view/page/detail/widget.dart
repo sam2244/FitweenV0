68b47d3c9d8e8b71d77b555c5675bfe5e1f3016d
YS-Jeong-99 <besthcy9908@gmail.com>
2022-07-07 14:05:22 +0900

    Upgrade detail.dart

--- lib/view/page/detail/widget.dart
+++ lib/view/page/detail/widget.dart
@@ -6,114 +6,58 @@ import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:weekly_date_picker/weekly_date_picker.dart';
 import '../main/trainer/widget.dart';
 
-class TraineeInfo extends StatelessWidget {
+class TraineeDetailPage extends StatelessWidget {
   final Trainee trainee;
-  const TraineeInfo({Key? key, required this.trainee}) : super(key: key);
+  const TraineeDetailPage({Key? key, required this.trainee}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<TrainerDetailPresenter>(
-      builder: (controller) {
-        return Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
-                height: 124.0,
-                child: Row(
-                  children: [
-                    const TraineeProfileImage(),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          FWText(
-                            'D - 60',
-                            style: Theme.of(context).textTheme.titleSmall,
-                          ),
-                          const SizedBox(height: 8.0),
-                          FWText(
-                            trainee.name,
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TraineeInformation(trainee: trainee),
+          const TraineeWeeklyCalendar(),
+          const TraineeExerciseCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class TraineeInformation extends StatelessWidget {
+  final Trainee trainee;
+  const TraineeInformation({Key? key, required this.trainee}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
+      height: 124.0,
+      child: Row(
+        children: [
+          const TraineeProfileImage(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FWText(
+                  'D - 60',
+                  style: Theme.of(context).textTheme.titleSmall,
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: WeeklyDatePicker(
-                  selectedDay: controller.selectedDay,
-                  changeDay: (selected) => controller.indexChanged(selected),
-                  // selectedBackgroundColor: Colors.amberAccent,
+                const SizedBox(height: 8.0),
+                FWText(
+                  trainee.name,
+                  style: Theme.of(context).textTheme.titleMedium,
+                  color: Theme.of(context).colorScheme.onSurface,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4.0),
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12.0, horizontal: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        FWText(
-                          "운동",
-                          style: Theme.of(context).textTheme.titleMedium,
-                          color: Theme.of(context).colorScheme.onSurface,
-                        ),
-                        Divider(
-                          color: Theme.of(context).colorScheme.outline,
-                        ),
-                        ListTile(
-                          leading: Checkbox(
-                            value: controller.checkedState,
-                            onChanged: (value) =>
-                                controller.checkboxToggle(value!),
-                          ),
-                          title: FWText(
-                            '윗몸 일으키기',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                            color: Theme.of(context).colorScheme.onSurface,
-                          ),
-                        ),
-                        ListTile(
-                          leading: Checkbox(
-                            value: controller.checkedState,
-                            onChanged: (value) =>
-                                controller.checkboxToggle(value!),
-                          ),
-                          title: FWText(
-                            '윗몸 일으키기',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                            color: Theme.of(context).colorScheme.onSurface,
-                          ),
-                        ),
-                        ListTile(
-                          leading: Checkbox(
-                            value: controller.checkedState,
-                            onChanged: (value) =>
-                                controller.checkboxToggle(value!),
-                          ),
-                          title: FWText(
-                            '윗몸 일으키기',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                            color: Theme.of(context).colorScheme.onSurface,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
@@ -142,6 +86,85 @@ class TraineeProfileImage extends StatelessWidget {
         colors: [Color(0xffB07BE6), Color(0xff5BA2E0)],
       ),
       circularStrokeCap: CircularStrokeCap.round,
+    );
+  }
+}
+
+class TraineeWeeklyCalendar extends StatelessWidget {
+  const TraineeWeeklyCalendar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<TrainerDetailPresenter>(
+      builder: (controller) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: WeeklyDatePicker(
+            selectedDay: controller.selectedDay,
+            changeDay: (selected) => controller.indexChanged(selected),
+            selectedBackgroundColor: Theme.of(context).colorScheme.primary,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class TraineeExerciseCard extends StatelessWidget {
+  const TraineeExerciseCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Map? sameDay;
+    int? sanDayIndex;
+
+    return GetBuilder<TrainerDetailPresenter>(
+      builder: (controller) {
+        sameDay = controller.demo.firstWhereOrNull(
+          (element) =>
+              element['dateTime'].year == controller.selectedDay.year &&
+              element['dateTime'].month == controller.selectedDay.month &&
+              element['dateTime'].day == controller.selectedDay.day,
+        );
+        sameDay != null ? sanDayIndex = controller.demo.indexOf(sameDay!) : 0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Card(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FWText(
+                    "운동",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                  Divider(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                  if (sameDay != null)
+                    for (int i = 0; i < sameDay!['exercises'].length; i++)
+                      ListTile(
+                        leading: Checkbox(
+                          value: sameDay!['completed'][i],
+                          onChanged: (value) =>
+                              controller.checkboxState(sanDayIndex!, i, value!),
+                        ),
+                        title: FWText(
+                          sameDay!['exercises'][i],
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
